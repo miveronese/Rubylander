f927bd50375088c57c9abd1cb7e5e598cff0978e
michelle <michelle@Razorblade.local>
2014-07-10 17:56:26 +0200

    improved layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
 
-
-  root 'tutorial#run'
-  post '/' => 'tutorial#run'
-
-
-
-  #resources :lessons
+root 'tutorial#run'
+post '/' => 'tutorial#run'
+  # resources :lessons
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
