1419f50c22a66818cba9be1538a753c89a12f15e
Caroline Visoto Nunes <carolvnunes@Carolines-MacBook-Air.local>
2014-07-17 15:14:03 +0200

    tutorial controller update (exceptions)

--- app/controllers/tutorial_controller.rb
+++ app/controllers/tutorial_controller.rb
@@ -1,10 +1,22 @@
 class TutorialController < ApplicationController
 
   def run
-    @input = params["text"].to_s
-    puts @input.inspect
-    @result = eval(@input).to_s
+    begin
+      @input  = params["text"].to_s
+      @result = check_input(@input)
+    rescue SyntaxError, NameError => syntax_name_error
+      @result = " String doesn't compile: " + syntax_name_error.to_s
+      puts      " _____String doesn't compile____: " + syntax_name_error.to_s
+    rescue StandardError => standard_error
+      @result = " Error running script: " + standard_error.to_s
+      puts      " Error running script: " + standard_error.to_s
+    end
 
+  end
+
+
+  def check_input(input)
+   raise  "Empty value is not correct, please try again" if @input.nil? else eval(input)
   end
 
 end
