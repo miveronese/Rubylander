39474a91e3dfabd6de9dc6e204af9c59e7663691
michelle <michelle@Razorblade.local>
2014-07-21 16:23:46 +0200

    exceptions + evil codes in two functions

--- app/controllers/tutorial_controller.rb
+++ app/controllers/tutorial_controller.rb
@@ -1,22 +1,69 @@
 class TutorialController < ApplicationController
 
-  def run
-    begin
-      @input  = params["text"].to_s
-      @result = check_input(@input)
-    rescue SyntaxError, NameError => syntax_name_error
-      @result = " String doesn't compile: " + syntax_name_error.to_s
-      puts      " _____String doesn't compile____: " + syntax_name_error.to_s
-    rescue StandardError => standard_error
-      @result = " Error running script: " + standard_error.to_s
-      puts      " Error running script: " + standard_error.to_s
-    end
 
+
+
+#   def run
+#     @input = params["text"].to_s
+#     @result = emptyvalue(@input)
+#
+#   end
+#
+#   def evilcode(input)
+#   ["bli", "ble"].each do |x|
+#       if @input.include?(x)
+#         raise "hacker"
+#       else
+#       eval (input)
+#       end
+#     end
+#   end
+#
+#
+#
+#   def emptyvalue(input)
+#     if @input.empty?
+#       raise "Empty value, try again"
+#     else
+#       evilcode (input)
+#     end
+#   end
+#
+#
+#
+#
+# end
+
+
+
+  def check(input)
+    @evilcode = ["mkdir", "rmdir", "mydir", "rm", "ls", "-a", "ls -a", "cd", "pwd", "chroot", "cp", "-i", "cp -i",
+                 "`mdkdir`", "`rmdir`", "`mydir`", "`rm`", "`ls`", "`-a`", "`ls -a`", "`cd`", "`pwd`", "`chroot`", "`cp`", "`-i`", "`cp -i`"]
+
+    if @input.empty?
+      raise "Empty value, try again"
+    else
+      @evilcode.each do |x|
+        if @input.include?(x)
+          raise " hacker"
+        end
+      end
+      eval(input)
+    end
   end
 
 
-  def check_input(input)
-   raise  "Empty value is not correct, please try again" if @input.nil? else eval(input)
+  def run
+    begin
+      @input  = params["text"].to_s
+      @result = check(@input)
+    rescue SyntaxError => syntax_error
+      @result = " The syntax is not correct : " + syntax_error.message
+    rescue ArgumentError => argument_error
+      @result = " The argument is not correct : " + argument_error.message
+    rescue StandardError => standard_error
+      @result = " Error running script: " + standard_error.message
+    end
   end
 
 end
