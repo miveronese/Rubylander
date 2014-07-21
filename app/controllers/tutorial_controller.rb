1b6dd7f4fea388c1f6dc904abebcd1594e74d0ee
Duana Saskia <duana@soundcloud.com>
2014-07-21 17:51:42 +0200

    Refactored TutorialController with Duana

--- app/controllers/tutorial_controller.rb
+++ app/controllers/tutorial_controller.rb
@@ -1,69 +1,37 @@
 class TutorialController < ApplicationController
 
-
-
-
-#   def run
-#     @input = params["text"].to_s
-#     @result = emptyvalue(@input)
-#
-#   end
-#
-#   def evilcode(input)
-#   ["bli", "ble"].each do |x|
-#       if @input.include?(x)
-#         raise "hacker"
-#       else
-#       eval (input)
-#       end
-#     end
-#   end
-#
-#
-#
-#   def emptyvalue(input)
-#     if @input.empty?
-#       raise "Empty value, try again"
-#     else
-#       evilcode (input)
-#     end
-#   end
-#
-#
-#
-#
-# end
-
-
-
-  def check(input)
-    @evilcode = ["mkdir", "rmdir", "mydir", "rm", "ls", "-a", "ls -a", "cd", "pwd", "chroot", "cp", "-i", "cp -i",
-                 "`mdkdir`", "`rmdir`", "`mydir`", "`rm`", "`ls`", "`-a`", "`ls -a`", "`cd`", "`pwd`", "`chroot`", "`cp`", "`-i`", "`cp -i`"]
+  def run
+    @input  = params["text"].to_s
 
     if @input.empty?
-      raise "Empty value, try again"
+      @result = "Empty value, try again"
+    elsif contains_evil_codes?(@input)
+      @result = "You are a hacker"
     else
-      @evilcode.each do |x|
-        if @input.include?(x)
-          raise " hacker"
-        end
+      @result = evaluate(@input)
+    end
+  end
+
+  def contains_evil_codes?(input)
+    evilcode = ["mkdir", "rmdir", "mydir", "rm", "ls", "-a", "ls -a", "cd", "pwd", "chroot", "cp", "-i", "cp -i",
+      "`mkdir`", "`rmdir`", "`mydir`", "`rm`", "`ls`", "`-a`", "`ls -a`", "`cd`", "`pwd`", "`chroot`", "`cp`", "`-i`", "`cp -i`"]
+    evilcode.each do |x|
+      if input.include?(x)
+        return true
       end
-      eval(input)
     end
+    false
   end
 
-
-  def run
+  def evaluate(input)
     begin
-      @input  = params["text"].to_s
-      @result = check(@input)
+      eval(input)
     rescue SyntaxError => syntax_error
-      @result = " The syntax is not correct : " + syntax_error.message
+      " The syntax is not correct : " + syntax_error.message
     rescue ArgumentError => argument_error
-      @result = " The argument is not correct : " + argument_error.message
+      " The argument is not correct : " + argument_error.message
     rescue StandardError => standard_error
-      @result = " Error running script: " + standard_error.message
+      "Error running script: " + standard_error.message
     end
   end
-
 end
