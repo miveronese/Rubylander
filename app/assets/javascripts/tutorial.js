763cfdae55a1c369fbfdc17b4edd05127f353d9f
michelle <michelle@Razorblade.local>
2014-08-21 16:10:42 +0200

    next button + layout changes

--- app/assets/javascripts/tutorial.js
+++ app/assets/javascripts/tutorial.js
@@ -1,15 +1,23 @@
+
+var greet = {
+  welcome: "Welcome to RubyLander!"
+};
+
+var start = {
+  preface: "It's time to learn Ruby. balblalblaal balblalblaal balblalblaal balblalblaal balblalblaal"
+};
+
 var tutorial = {
-  title: "Ruby Values",
+  title: "",
   language: "ruby",
   steps: [
     {
-      //"Javascript values are either numbers, strings, objects, arrays, booleans." +
       message: "Let's start with numbers. Numbers can be integers like 1, 2 or 5000 or floating point." +
         "For example 1.1, 2.111 and so forth. Try to add 1 + 2.",
       result: 3
     },
     {
-      message: "OK, great. How about adding two floats 1.1 and 2.3",
+      message: "OK, great. How about adding two floats 1.1 and 2.3?",
       result: 3.4
     },
     {
@@ -21,65 +29,57 @@ var tutorial = {
 
 
 $(function () {
-  var jqconsole = $('#console').jqconsole(tutorial.title + '\n', '>>> ');
-  var lastResult;
 
-  var jsrepl = new JSREPL({  
-    input: function() {   
-      //console.log("INPUT WANTED");     
-    },  
-    output: function(s) {
-      jqconsole.Write(s + '\n', 'jqconsole-output');
-    },
-    result: function(result) {
-      lastResult = result;
-      jqconsole.Write(result + '\n', 'jqconsole-result');
-    }
-    
-    // error: errorCallback,  
-    // progress: progressCallback,  
-    // timeout: {  
-    //   time: 30000,  
-    //   callback: timeoutCallback  
-    // }  
-  }); 
+      $('#messages').text(start.preface);
 
-  // this is the main loop which gets called from the loadLanguage callback below
-  // the parameter step show
-  function runStep(stepNumber) {
-    // get the object for the step for the given step number
-    // so it contains { message: "....", result: ... }
-    var step = tutorial.steps[stepNumber];
+      $('#submit').click(function(){
+          $(this).hide();
 
-    // Print the message
-    $('div#messages').text(step.message);
 
-    // when the user hits enter the following function gets called
-    jqconsole.Prompt(true, function (input) {
+        var jqconsole = $('#console').jqconsole(greet.welcome + '\n', '>>> ');
+        var lastResult;
 
-      // the parameter input contains the user input
-      // evaluate the input with the language interpreter
-      jsrepl.eval(input);
+        var jsrepl = new JSREPL({  
+          input: function() {   
+          },  
+          output: function(s) {
+            jqconsole.Write(s + '\n', 'jqconsole-output');
+          },
+          result: function(result) {
+            lastResult = result;
+            jqconsole.Write(result + '\n', 'jqconsole-result');
+          } 
+        }); 
 
-      // running eval triggers the result function above
-      // lastResult contains now the result of the eval
-      setTimeout(function() {
-        if (lastResult == step.result) {
-           runStep(stepNumber + 1);          
-        } else {
-          jqconsole.Write("Sorry is not correct. Please try again. \n")
-          runStep(stepNumber)
-        }        
-      }, 100);
-    });
-  }
 
-  // Load the actual ruby interpreter which can take a while
-  // the following function gets called when the interpreter is loaded
-  jsrepl.loadLanguage(tutorial.language, function () {  
-    jqconsole.Write('Interpreter loaded\n\n');
+        function runStep(stepNumber) {
+          var step = tutorial.steps[stepNumber];
+         
+          $('#messages').text(step.message);
 
-    // start the user interaction
-    runStep(0);
-  });
+          jqconsole.Prompt(true, function (input) {
+            jsrepl.eval(input);
+
+            setTimeout(function() {
+              if (lastResult == step.result) {
+                 runStep(stepNumber + 1);          
+              } else {
+                jqconsole.Write("Oops, try again. \n")
+                runStep(stepNumber)
+              }        
+            }, 100);
+          });
+        }
+
+
+        jsrepl.loadLanguage(tutorial.language, function () {  
+        jqconsole.Write(':) \n\n');
+        
+          runStep(0);
+        });
+
+      });
+
+
 });
+    
