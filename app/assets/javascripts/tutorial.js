55edd3b810b9a613fd3d9d538ac2bb7de497122b
michelle <michelle@Razorblade.local>
2014-09-16 14:50:40 +0200

    trying to use anchor

--- app/assets/javascripts/tutorial.js
+++ app/assets/javascripts/tutorial.js
@@ -1,3 +1,28 @@
+
+// <button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+//   Launch demo modal
+// </button>
+
+// <!-- Modal -->
+// <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+//   <div class="modal-dialog">
+//     <div class="modal-content">
+//       <div class="modal-header">
+//         <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+//         <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+//       </div>
+//       <div class="modal-body">
+//         ...
+//       </div>
+//       <div class="modal-footer">
+//         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+//         <button type="button" class="btn btn-primary">Save changes</button>
+//       </div>
+//     </div>
+//   </div>
+// </div>
+
+
 var FIRST_STEP = 0;
 var FIRST_LESSON = 0;
 var LAST_STEP_OF_FIRST_LESSON = 1;
@@ -6,6 +31,10 @@ function next(id) { return id + 1; }
 
 function hideButton() {
     $("#button").hide();
+}
+
+function Glossary() {
+    $("#glossary").html(); 
 }
 
 function createJqconsole() {
