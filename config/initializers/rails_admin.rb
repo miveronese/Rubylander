ae3975de0d4fc422117216562b774f9901130ba4
michelle <michelle@Razorblade.local>
2014-09-25 15:05:48 +0200

    testing 3

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -8,7 +8,7 @@ RailsAdmin.config do |config|
   
   config.authorize_with do
     authenticate_or_request_with_http_basic('Login required') do |username, password|
-    name == Rails.application.secrets.name && password == Rails.application.secrets.password
+    username == Rails.application.secrets.user && password == Rails.application.secrets.password
     end
   end
 
