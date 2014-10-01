4be2bf32d2601fb45c82afbc57f0d16a2163c07f
Caroline Visoto Nunes <carolvisoto@gmail.com>
2014-10-01 18:19:55 +0200

    update the line position in steps

--- app/assets/javascripts/tutorial.js
+++ app/assets/javascripts/tutorial.js
@@ -4,13 +4,16 @@ var LAST_STEP_OF_FIRST_LESSON = 1;
 var console;
 var repl;
 
-function next(id) { return id + 1; }
+function next(id) {
+    return id + 1;
+}
 
 function hideButton() {
-   $("#button").hide();
+    $("#button").hide();
 }
-function hideSummary (){
-   $("#summary").hide();
+
+function hideSummary() {
+    $("#summary").hide();
 }
 
 function createJqconsole() {
@@ -20,22 +23,23 @@ function createJqconsole() {
 function createJsRepl() {
     return new JSREPL();
 }
-function checkAddress(lessonNumber){
+
+function checkAddress(lessonNumber) {
     var address = "";
-     if(lessonNumber == 0){
-         address = "/lessons/first"; 
-     }else{
-         address = "/lessons/" + lessonNumber;
-     }   
-    return address; 
+    if (lessonNumber == 0) {
+        address = "/lessons/first";
+    } else {
+        address = "/lessons/" + lessonNumber;
+    }
+    return address;
 }
 
 function loadLesson(lessonNumber, runStep) {
 
-    $.getJSON(checkAddress(lessonNumber), function(lesson){
+    $.getJSON(checkAddress(lessonNumber), function(lesson) {
         setLessonTitle(lesson.title);
         runStep(lesson, FIRST_STEP);
-    });   
+    });
 }
 
 function loadRubyLanguage(repl, languageCallback) {
@@ -48,12 +52,17 @@ function setLessonTitle(title) {
     $('#lesson_title').html(title);
 }
 
-function applyGlossaryTips(){
-    $('.glossary-tips').tooltip({placement: 'top'});
+function applyGlossaryTips() {
+    $('.glossary-tips').tooltip({
+        placement: 'top'
+    });
 }
 
-function applyGlossaryPopover(){
-    $('.glossary-popover').popover({trigger:'hover', placement:'top'});
+function applyGlossaryPopover() {
+    $('.glossary-popover').popover({
+        trigger: 'hover',
+        placement: 'top'
+    });
 }
 
 function showStep(message) {
@@ -77,8 +86,8 @@ function showNextLesson(nextLessonId, runStep) {
 function summarylesson() {
     hideSummary();
     var createStepItem = function(step) {
-        var stepItem = $("<li>");    
-        stepItem.append(step.title);        
+        var stepItem = $("<li>");
+        stepItem.append(step.title);
         return stepItem;
     };
     var createTitle = function(lesson) {
@@ -88,62 +97,69 @@ function summarylesson() {
     };
 
     $.getJSON("/lessons/", function(lessons) {
-           var accordionDiv = $("#accordion");  
-            lessons.forEach (function(lesson) {            
-            accordionDiv.append(createTitle(lesson)); 
+        var accordionDiv = $("#accordion");
+        lessons.forEach(function(lesson) {
+            accordionDiv.append(createTitle(lesson));
             var sizeOfStep = lesson.steps.length
- 
+
             var stepsContainer = $("<div>");
             accordionDiv.append(stepsContainer);
             var stepsList = $("<ul>");
-            stepsContainer.append(stepsList);            
-            
-            lesson.steps.forEach(function(step){
-              stepsList.append(createStepItem(step));  
+            stepsContainer.append(stepsList);
+
+            lesson.steps.forEach(function(step) {
+                stepsList.append(createStepItem(step));
             });
-            
+
         });
 
-        accordionDiv.accordion({ header: "h3", collapsible: true, active: false, heightStyle:"content"}); 
+        accordionDiv.accordion({
+            header: "h3",
+            collapsible: true,
+            active: false,
+            heightStyle: "content"
+        });
     });
 
 
-     $("#summary").show();
-     $("#summary").click(function() {
-        $("#summary-box").modal({show: true});
-      });
+    $("#summary").show();
+    $("#summary").click(function() {
+        $("#summary-box").modal({
+            show: true
+        });
+    });
 }
 
-function evaluateRailsAdminResults(lesson, stepNumber){
- var step = lesson.steps[stepNumber];
+function evaluateRailsAdminResults(lesson, stepNumber) {
+    var step = lesson.steps[stepNumber];
 
- var resultFunction;
+    var resultFunction;
     try {
-        resultFunction = eval("(" + step.result + ")");  
+        resultFunction = eval("(" + step.result + ")");
         if (typeof(resultFunction) != "function") {
-            console.Write("The result function for this step is not a function. Step id:" + step.id+ "\n");
+            console.Write("The result function for this step is not a function. Step id:" + step.id + "\n");
             // prompt again
             runStep(lesson, stepNumber);
-        }                                         
-    } catch(err) {
-       console.Write("The result function for this step has a syntax error. Step id: " + step.id + ": " + err +"\n");
-       runStep(lesson, stepNumber);
-    }  
+        }
+    } catch (err) {
+        console.Write("The result function for this step has a syntax error. Step id: " + step.id + ": " + err + "\n");
+        runStep(lesson, stepNumber);
+    }
     return resultFunction;
 }
 
 function runStep(lesson, stepNumber) {
-    var step = lesson.steps[stepNumber];    
-   
+    var step = lesson.steps[stepNumber];
+
     if (lesson.id == FIRST_LESSON && stepNumber == LAST_STEP_OF_FIRST_LESSON) {
-        loadLesson(next(lesson.id), runStep);  
+        loadLesson(next(lesson.id), runStep);
     } else if (stepNumber == lesson.steps.length) {
         showNextLesson(next(lesson.id), runStep);
     } else {
 
         showStep(step.text);
 
-        console.Prompt(true, function (input) {
+        console.Prompt(true, function(input) {
             repl.once("error", function(e) {
                 console.Write(e);
                 runStep(lesson, stepNumber);
@@ -152,18 +168,18 @@ function runStep(lesson, stepNumber) {
 
             repl.once("result", function(result) {
                 console.Write(result + '\n', 'jqconsole-result');
-                var resultFunction = evaluateRailsAdminResults(lesson, stepNumber); 
+                var resultFunction = evaluateRailsAdminResults(lesson, stepNumber);
 
                 // if result of user's command was nil
                 if (result == "nil") {
                     // prompt again
                     runStep(lesson, stepNumber);
 
-                }else{
+                } else {
                     // check for correct result
                     if (resultFunction(eval(result))) {
-                      //go to the next step  
-                      runStep(lesson, stepNumber + 1);
+                        //go to the next step  
+                        runStep(lesson, stepNumber + 1);
                     } else {
                         //prompt again
                         runStep(lesson, stepNumber);
@@ -181,7 +197,7 @@ function runStep(lesson, stepNumber) {
 //comentario para testar gihub
 
 function startTutorial() {
-    
+
     // setup the lesson panel
     hideButton();
     summarylesson();
@@ -190,17 +206,17 @@ function startTutorial() {
     console = $('#console').jqconsole("Welcome to RubyLander!\n", '>>> ');
 
     // customize console
-     $(window).click(function() {  
-       console.Focus();  
-     })
-    
+    $(window).click(function() {
+        console.Focus();
+    })
+
     // create repl
     repl = new JSREPL();
-        
+
     // load language
     repl.loadLanguage("ruby", function() {
-        console.Write(":) \n" );
+        console.Write(":) \n");
         loadLesson(FIRST_LESSON, runStep);
     });
-    
+
 }
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140825150230) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "courses", force: true do |t|
     t.string   "language"
     t.string   "programming_language"
