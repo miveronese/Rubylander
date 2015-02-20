3e547d3502c1d435021b0d76f8d4792e06d86b27
Caroline Visoto Nunes <carolvisoto@gmail.com>
2015-02-20 17:03:12 +0100

    create new branch to link steps on accordion

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -5,20 +5,20 @@ class LessonsController < ApplicationController
   	@course = Course.first
   end
 
-	def index
-		@all_lessons = Lesson.all
-		render json: @all_lessons, include: :steps
-	end
+  def index
+	@all_lessons = Lesson.all
+	render json: @all_lessons, include: :steps
+  end
 
-	def show
-		@lesson = Lesson.find(params[:id])
-		render json: @lesson, include: :steps
-	end
+  def show
+	@lesson = Lesson.find(params[:id])
+	render json: @lesson, include: :steps
+  end
 
-	def first
-        @first_lesson = Lesson.first
-        render json: @first_lesson, include: :steps
-	end
+  def first
+    @first_lesson = Lesson.first
+    render json: @first_lesson, include: :steps
+  end
 
 
 end
